39f316e45448aef253c1c1d6c9574b7ade406bb5
Kirill Volkov <moonvolk4@Kirills-MacBook-Pro.local>
2025-12-02 15:57:44 +0300

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/HypertensionStageClassification-frontend/',
   server: {
     port: 3000,
     proxy: {
